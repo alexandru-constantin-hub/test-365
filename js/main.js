a6fbc546e82fa848b0d25fffe63b0329ac41cce1
Alexandru Constantin <alexandru.constantin@live.com>
2021-09-29 14:23:18 -0400

    finished

---
+++ js/main.js
@@ -0,0 +1,84 @@
+$(function() {
+    // toggle du btn active
+    $('[data-filter-color]').click(function() {
+         var choosenColor = $(this).data('filter-color');
+
+        $('[data-filter-color]').each(function(e){
+            $(this).removeClass('active');
+        });
+        $(this).addClass('active');
+
+        //toggle des sliders
+        $('[data-slider]').each(function(e){
+            if($(this).data('slider') = choosenColor){
+                $(this).addClass('active');
+                $(this).slick('setPosition');
+            }else{
+                $(this).removeClass('active');
+            }
+        });
+        return false;
+    });
+});
+
+$(document).ready(function(){
+    // slider
+    let slideWrapper = $(".slider-section__slider");
+
+    slideWrapper.slick({
+        fade:true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        centerMode: true,
+    });
+
+    // toggle menu mobile
+   /* $('[ data-mobile-menuburger-open]').click(function() {
+        $('.header-mobile__menu').css("display", "block");
+    });
+
+    $('[ data-mobile-menuburger-close]').click(function() {
+        $('.header-mobile__menu').css("display", "none");
+    });
+    */
+
+    $( ".header-mobile__burger" ).click(function() {
+        $( ".header-mobile__menu" ).slideToggle( "slow" );
+      });
+
+    // toggle le sous menu
+    // $('[ data-menu-dropdown]').click(function() {
+    //     if($(this).find('.dropdown').hasClass('dropdown--opened')){
+    //         $(this).find('.dropdown').removeClass('dropdown--opened');
+    //         $(this).find('.header-mobile__link').removeClass('header-mobile__link--opened');
+    //     }else{
+    //         $(this).find('.dropdown').addClass('dropdown--opened');
+    //         $(this).find('.header-mobile__link').addClass('header-mobile__link--opened');
+    //     }
+    // });
+     
+
+    $( ".header-mobile__has-dropdown" ).click(function() {
+        $( ".header-mobile__has-dropdown .dropdown" ).slideToggle( "slow" );
+      });
+
+     
+    $(".slick-dots li button").eq(0).text("Rouge");
+    $(".slick-dots li button").eq(1).text("Vert");
+    $(".slick-dots li button").eq(2).text("Bleu");
+
+    // $( ".slick-dots li button" ).click(function(event) {
+    //     alert(event.target.id);
+    //     $targetButton = event.target.id;
+    //     $targetButton.css( 'background-color', '#666666' );
+    //   });
+
+
+});
+
+
